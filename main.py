7b1f9dd11f4943fb343430e735a3164537b0b09e
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2022-03-12 14:41:34 -0500

    init project layout

---
+++ main.py
@@ -0,0 +1,16 @@
+# project sketch
+
+# local setup
+#   read env
+#   init raw data cache
+#   init refined metadata cache
+
+# remote fetch
+#   fetch raw data
+#   extract metadata
+
+# client management
+#   init paused
+#   apply filters
+#   modify paused
+#   resume
